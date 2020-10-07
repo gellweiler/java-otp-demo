7ff93820722aa910a8ca9c8aa7e031d63e2f90e8
Sebastian Gellweiler <gellweiler@craft-coders.de>
2020-10-07 23:22:29 +0200

    Extended program to support vigenere and added more tests.

--- src/main/java/net/gellweiler/otp/OTP.java
+++ src/main/java/net/gellweiler/otp/OTP.java
@@ -3,29 +3,57 @@ package net.gellweiler.otp;
 public class OTP {
     public static String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    /**
+     * Shift the given character in text by offset.
+     */
+    public static char shiftChar(char text, int offset) {
+        int newIndex = ALPHABET.indexOf(text) + offset;
+
+        if (newIndex >= ALPHABET.length()) {
+            newIndex = newIndex - ALPHABET.length();
+        } else if (newIndex < 0) {
+            newIndex = newIndex + ALPHABET.length();
+        }
+
+        return ALPHABET.charAt(newIndex);
+    }
+
+    /**
+     * Encrypt the text character with the key character.
+     */
+    public static char encryptChar(char text, char key) {
+        return shiftChar(text, ALPHABET.indexOf(key));
+
+    }
+
+    /**
+     * Decrypt the text character with the key character.
+     */
+    public static char decryptChar(char text, char key) {
+        return shiftChar(text, -ALPHABET.indexOf(key));
+    }
+
+    /**
+     * Encrypt text with key.
+     */
     public static String encrypt(String key, String text) {
         String output = "";
 
         for (int i = 0; i < text.length(); i++) {
-            char cKey = key.charAt(i);
-            char cText = text.charAt(i);
-
-            int newIndex = (ALPHABET.indexOf(cText) + ALPHABET.indexOf(cKey)) % ALPHABET.length();
-            output += ALPHABET.charAt(newIndex);
+            output += encryptChar(text.charAt(i), key.charAt(i % key.length()));
         }
 
         return output;
     }
 
+    /**
+     * Decrypt text with key.
+     */
     public static String decrypt(String key, String text) {
         String output = "";
 
         for (int i = 0; i < text.length(); i++) {
-            char cKey = key.charAt(i);
-            char cText = text.charAt(i);
-
-            int newIndex = (ALPHABET.indexOf(cText) - ALPHABET.indexOf(cKey) + ALPHABET.length()) % ALPHABET.length();
-            output += ALPHABET.charAt(newIndex);
+            output += decryptChar(text.charAt(i), key.charAt(i % key.length()));
         }
 
         return output;
--- src/test/java/net/gellweiler/otp/OTPTest.java
+++ src/test/java/net/gellweiler/otp/OTPTest.java
@@ -8,7 +8,40 @@ import static org.junit.Assert.*;
 public class OTPTest {
 
     @Test
-    public void encrypt() {
+    public void shiftCharSimple() {
+        Assert.assertEquals('B', OTP.shiftChar('A', 1));
+        Assert.assertEquals('D', OTP.shiftChar('A', 3));
+        Assert.assertEquals('X', OTP.shiftChar('X', 0));
+    }
+
+    @Test
+    public void shiftCharWithOverflow() {
+        Assert.assertEquals('A', OTP.shiftChar('Z', 1));
+        Assert.assertEquals('B', OTP.shiftChar('Y', 3));
+    }
+
+    @Test
+    public void shiftCharWithUnderflow() {
+        Assert.assertEquals('Z', OTP.shiftChar('A', -1));
+        Assert.assertEquals('Y', OTP.shiftChar('B', -3));
+    }
+
+    @Test
+    public void encryptChar() {
+        Assert.assertEquals('D', OTP.encryptChar('D', 'A'));
+        Assert.assertEquals('A', OTP.encryptChar('Z', 'B'));
+        Assert.assertEquals('B', OTP.encryptChar('X', 'E'));
+    }
+
+    @Test
+    public void decryptChar() {
+        Assert.assertEquals('D', OTP.decryptChar('D', 'A'));
+        Assert.assertEquals('Z', OTP.decryptChar('A', 'B'));
+        Assert.assertEquals('X', OTP.decryptChar('B', 'E'));
+    }
+
+    @Test
+    public void encryptOTP() {
         String secret = "GEHEIMNIS";
         String key    = "ABCDEFGHI";
 
@@ -18,9 +51,29 @@ public class OTPTest {
     }
 
     @Test
-    public void decrypt() {
+    public void decryptOTP() {
         String key    = "ABCDEFGHI";
         String encrypted = "GFJHMRTPA";
+
+        String secret = OTP.decrypt(key, encrypted);
+
+        Assert.assertEquals("GEHEIMNIS", secret);
+    }
+
+    @Test
+    public void encryptVigenere() {
+        String secret = "GEHEIMNIS";
+        String key    = "ABC";
+
+        String encrypted = OTP.encrypt(key, secret);
+
+        Assert.assertEquals("GFJEJONJU", encrypted);
+    }
+
+    @Test
+    public void decryptVigenere() {
+        String encrypted = "GFJEJONJU";
+        String key    = "ABC";
 
         String secret = OTP.decrypt(key, encrypted);
 
